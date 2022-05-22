b9a0c424cd3c0a70bdf1802775ac5c4781a6bb9d
Firdaus Huawei <firdauskhamid01@gmail.com>
2022-05-22 15:45:43 +0800

    "Added counter in bubble sort"

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -8,19 +8,21 @@ class BubbleSort extends Words {
 
     public void bubbleSort() throws IOException {
         String[] WordList = getWords().toArray(new String[0]);
-        OperationsCounter++;
+        OperationsCounter += 2; // Assgnmnt & Referencing
 
         System.out.println("Starting the bubble sort algorithm ...");
-
+        OperationsCounter++; // int j = 1
         for (int j = 0; j < WordList.length - 1; j++) {
-            OperationsCounter += 2;
+            OperationsCounter += 4; // j < WordList.length - 1 and j++
+            OperationsCounter += 2; // int i = j + 1
             for (int i = j + 1; i < WordList.length; i++) {
-                OperationsCounter += 2;
+                OperationsCounter += 3; // i < WordList.length and i++
+                OperationsCounter += 3; // WordList[j].compareToIgnoreCase(WordList[i]) > 0
                 if (WordList[j].compareToIgnoreCase(WordList[i]) > 0) {
                     String temp = WordList[j];
                     WordList[j] = WordList[i];
                     WordList[i] = temp;
-                    OperationsCounter += 3;
+                    OperationsCounter += 7; // All operations in this if statement
                 }
             }
         }
@@ -28,8 +30,12 @@ class BubbleSort extends Words {
         System.out.println("The Words List has been sorted using Bubble Sort, now writing it into a text file ...");
 
         FileWriter writer = new FileWriter("bubbleSorted.txt");
+        OperationsCounter++; // Assignment
+        OperationsCounter++; // int x = 0
         for (int x = 0; x < WordList.length; x++) {
+            OperationsCounter += 3; // For loop.
             writer.write(WordList[x] + '\n');
+            OperationsCounter += 1; // Referencing
         }
         writer.close();
         System.out.println("The Sorted Words List is now in bubbleSorted.txt file");
