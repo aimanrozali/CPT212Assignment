c7c1f8b25600a07994e07b31d939bc583ecc0752
Firdaus Huawei <firdauskhamid01@gmail.com>
2022-05-22 13:47:49 +0800

    "Added counter, but unfinished in recursive"

--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -3,6 +3,7 @@ package com.company;
 import java.io.FileWriter;
 import java.io.IOException;
 
+// Quick Sort Algorithm
 class QuickSort extends Words {
     static int OperationsCounter = 0;
 
@@ -26,53 +27,52 @@ class QuickSort extends Words {
             throws IOException {
 
         int i = lowerindex; // low index
-
         int j = higherindex; // high index
-
+        OperationsCounter += 2; // 2 assignments
 
         String pivot = WordList[i + (j - i) / 2];
+        OperationsCounter += 5; // Assignment, Referencing & Arithmetic
 
+        OperationsCounter++; // First comparison
         while (i <= j)
-
         {
+            OperationsCounter++;//Loop comparison
+
+            OperationsCounter++;//First comparison
             while (WordList[i].compareToIgnoreCase(pivot) < 0)
-
             {
+                OperationsCounter++;//Loop comparison
                 i++;
-
+                OperationsCounter += 2; // Assignment & Arithmetic
             }
+            OperationsCounter++;//Entrance comparison
             while (WordList[j].compareToIgnoreCase(pivot) > 0)
-
             {
+                OperationsCounter++;//Entrance comparison
                 j--;
-
+                OperationsCounter += 2; // Assignment & Arithmetic
             }
+            OperationsCounter++;//Entrance comparison
             if (i <= j)
-
             {
                 String temp = WordList[i];
-
+                OperationsCounter += 2; // Asgnmnt + Referencing
                 WordList[i] = WordList[j];
-
+                OperationsCounter += 3; // Asgnmnt + 2 Referencing
                 WordList[j] = temp;
-
+                OperationsCounter += 3; // Asgnmnt + Referencing
                 i++;
-
                 j--;
-
-
+                OperationsCounter += 4; // Assignment & Arithmetic
             }
         }
-
+        OperationsCounter++;//Entrance comparison
         if (lowerindex < j) {
-
             quickSort(WordList, lowerindex, j, OperationsCounter);
         }
+        OperationsCounter++;//Entrance comparison
         if (i < higherindex) {
-
             quickSort(WordList, i, higherindex, OperationsCounter);
         }
-
     }
-
 }
